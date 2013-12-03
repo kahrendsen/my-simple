3f5a07a808cf325a9a319aa21220da0a87366b2e
MattLau <mattlau@outlook.com>
2013-12-02 19:44:46 -0600

    Update mysimpleDSL.scala

--- src/mysimpleDSL.scala
+++ src/mysimpleDSL.scala
@@ -1,14 +1,19 @@
 object Albert extends mysimpleDCG {
 
-  
-  
   def main(args:Array[String]) = {
-	declare int 'albert := 100 + 5
-	declare bool 'irvin := true
-	declare int 'kendall := 'albert + 5
-	declare float 'calvin := 1000
-	declare int 'lyee := 00000000100
-	declare string 'awesome := "happyyyyyy"
-	var x = 3
+       declare newVar 'matt := 10
+       declare newVar 'random := 'matt
+       declare newVar 'test := 'matt
+       declare newVar 'happen := "Ready"
+       declare function('matthew, 'justice)
+         'justice := 5
+         RETURN ('justice)
+         declare function('happy, 'sad)
+         endfunction()
+           PRINT_TYPES()
+       endfunction()
+       PRINT_TYPES()
+       var x = 3
+       
   }
 }
